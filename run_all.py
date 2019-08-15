8c4f81c3d4462100a109ed630a87d9e0153d6ff9
linshenping <lsp426892@#>
2019-08-15 09:57:01 +0800

    gengxin

--- run_all.py
+++ run_all.py
@@ -5,5 +5,5 @@ from HTMLTestReportCN import HTMLTestRunner
 suite = unittest.defaultTestLoader.discover("./")
 
 f = open("report.html", 'wb') # 二进制写格式打开要生成的报告文件
-HTMLTestRunner(stream=f,title="福建公共服务接口测试",description="包括厦门,福州,泉州,宁德",tester="linsp").run(suite)
+HTMLTestRunner(stream=f,title="福建公共服务接口测试",description="i厦门接口测试",tester="linsp").run(suite)
 f.close()
